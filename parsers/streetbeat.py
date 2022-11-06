8ab8936934ea15cce07886793e49001f77626fa3
kirka@revenuelab.biz <W0rk17Kirka>
2022-11-07 01:54:08 +0400

    Init

--- parsers/streetbeat.py
+++ parsers/streetbeat.py
@@ -1,52 +1,39 @@
 from database.requests import Request
+from enums.stores import Stores
+
 from selenium import webdriver
-from selenium_stealth import stealth
-import time
+from selenium.common import StaleElementReferenceException, NoSuchElementException
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from fake_useragent import UserAgent
-from enums.stores import Stores
-from multiprocessing import Pool
-import re
-from database.requests import Request
-import pickle
-from selenium.webdriver.support.ui import WebDriverWait as wait
-
-useragent = UserAgent()
-
-options = webdriver.ChromeOptions()
-#user-agent каждый раз рандомный юзер
-options.add_argument(f"user-agent={useragent.random}")
-
-#убрать уебищные уведомления
-prefs = {"profile.default_content_setting_values.notifications" : 2}
-options.add_experimental_option("prefs",prefs)
-
-#отключение что это автоматизированное ПО
-options.add_experimental_option(
-    "excludeSwitches", ['enable-automation'])
-#полное окно браузера
-options.add_argument("start-maximized")
-# headless mode
-
-#увеличение скорости
-options.add_argument('blink-settings=imagesEnabled=false')
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium_stealth import stealth
 
 
 class Streetbeat:
-    s = Service("C:\\Users\\Vlad\\Desktop\\megaparser\\chromedriver\\chromedriver.exe")
-    browser = webdriver.Chrome(service=s, options=options)
+    service = Service("../chromedriver/chromedriver")
 
-    stealth(browser,
+    desired_capabilities = DesiredCapabilities().CHROME
+    desired_capabilities["pageLoadStrategy"] = "none"
+
+    options = webdriver.ChromeOptions()
+    options.headless = True
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    options.add_argument("start-maximized")
+    options.add_argument("blink-settings=imagesEnabled=false")
+    browser = webdriver.Chrome(service=service, desired_capabilities=desired_capabilities, options=options)
+
+
+    stealth(
+        browser,
         languages=["en-US", "en"],
         vendor="Google Inc.",
         platform="Win32",
         webgl_vendor="Intel Inc.",
         renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True
-        )
-
-
+        fix_hairline=True,
+    )
 
     def start(self):
         for price in Request.find_prices(self, Stores.streetbeat.value):
@@ -55,13 +42,13 @@ class Streetbeat:
 
     def parser(self):
         Streetbeat.browser.get(self)
+        Streetbeat.browser.delete_all_cookies()
         Streetbeat.browser.add_cookie({'name': 'user_city', 'value': '%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0'})
-        time.sleep(10)
-        return print(Streetbeat.browser.find_element(By.TAG_NAME, "span").text)
+        price = WebDriverWait(
+            driver=Streetbeat.browser,
+            timeout=15,
+            ignored_exceptions=[NoSuchElementException, StaleElementReferenceException]
+        ).until(ec.presence_of_element_located((By.CLASS_NAME, "price-tag__default"))).get_attribute("innerHTML")
 
-
-
-
-
-
-
+        print("[SUCCESS]", self, price)
+        return price
